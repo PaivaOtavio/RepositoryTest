593863f2a914906615d63eeb6702531404ba1a42
Paiva Otávio <otavioc7874@gmail.com>
2024-03-05 15:04:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+alert('Boas vindas ao jogo do número secreto');
+
+let maxNumber = 2;
+let tentativas = 1;
+let chute;
+let numeroSecreto = parseInt(Math.random()*maxNumber+1) ;
+
+while(chute != numeroSecreto){
+    let chute = prompt(`Escolha um número entre 1 e ${maxNumber} `);
+if (chute == numeroSecreto) {
+    break
+}   else if(chute > numeroSecreto){
+        alert('O seu chute é maior que o número secreto')
+    }else{
+        alert('seu chute é menor que o número secreto')
+    }
+    tentativas++;
+}
+let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu. O número secreto é ${numeroSecreto}. Você precisou de ${tentativas} ${palavraTentativas} para acertar `);
+
+
+
